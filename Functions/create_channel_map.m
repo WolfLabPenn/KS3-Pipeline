f379f1add11c2822eca0d142c8876554d9538d38
mirzakhalili <mirzakh@umich.edu>
2023-09-20 13:34:12 -0400

    Update create_channel_map.m

--- Functions/create_channel_map.m
+++ Functions/create_channel_map.m
@@ -1,9 +1,9 @@
 function create_channel_map(map,bad_channels,x,y,name,path_to_save)
 chanMap=map(:);
 chanMap0ind=chanMap-1;
-xcoords(map,1)=x(:);
-ycoords(map,1)=y(:);
+xcoords=x(:);
+ycoords=y(:);
 kcoords=ones(length(map),1);
 connected=ones(length(map),1);
-connected(map(bad_channels))=0;
+connected(bad_channels)=0;
 save(fullfile(path_to_save,[name,'_channel_map']),'chanMap','chanMap0ind','xcoords','ycoords','kcoords','connected','name');
